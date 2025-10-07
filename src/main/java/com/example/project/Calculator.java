8ad164abbd60e13dd7d401640751335e7f0dadcd
Ayaan-Ramin <ayaanr@nycstudents.net>
2025-10-07 23:50:40 +0000

    message

--- src/main/java/com/example/project/Calculator.java
+++ src/main/java/com/example/project/Calculator.java
@@ -6,6 +6,8 @@ public class Calculator{
     //the calculator class has one attirbute -> a string that holds the model name of the calculator 
     public Calculator(String x) {
         //implement code here
+        calcModel = x;
+
     }
     
     // method that performs the operation designated by operand (which
@@ -16,39 +18,61 @@ public class Calculator{
     // REMINDER: use .equals to test if one string is equal to another (not ==)
     public double performOperation(String operand, int num1, int num2) {
         //implement code here
-        return 0.00;
+        double answer;
+        if (operand.equals("+")) {
+            answer = num1+num2;
+        } else if (operand.equals("-")) {
+            answer = num1-num2;
+        } else if (operand.equals("*")) {
+            answer = num1*num2;
+        } else if (operand.equals("/")) {
+            answer = num1/num2;
+        } else {
+            answer = 0;
+        }
+        return answer;
     }
 
     // method that checks if num1 is evenly divisible by num2, and if so,
     // returns true, and if not, returns false
     public boolean divisibleBy(int num1, int num2) {
         //implement code here
-        return false;
+        if (num1 % num2 == 0) {
+            return true;            
+        } else {
+            return false;
+        }
     }
 
     // method that constructs and returns a string representing a coordinate
     // pair in the format: "(x, y)"
     public String coordinatePair(int x, int y) {
-        //implement code here
-        return "";
+        return "(" + x + ", " + y + ")";
     }
     // method that determines and returns the result of |num1 - num2|
     public int absoluteValue(int num1, int num2) {
-        //implement code here
-        return 0;
+        int rawanswer = num1-num2;
+        if ( rawanswer < 0 ) {
+            rawanswer = rawanswer  *-1;
+        }
+        return rawanswer;
     }
 
     //method that returns the average of two numbers. 
     //the number returned should be rounded to the nearest tenth
     public double average(int num1, int num2){
-        return 0;
+        double average = num1+num2;
+        average = average/2.0;
+        double multipliedNumber = average * 10;
+        long roundedInteger = Math.round(multipliedNumber); 
+        double roundedToTenth = roundedInteger / 10.0; 
+        return roundedToTenth;
     }
     
     // method that constructs and returns a String in the format:
     // "This calculator is a model "+____+" where "+____+" should be filled in
     // with the value stored in the calcModel instance variable."
     public String info() {
-        //implement code here
-        return "";
+        return "This calculator is a model "+calcModel+" where "+calcModel+" should be filled in with the value stored in the calcModel instance variable.";
     }
 }
--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -5,8 +5,18 @@ public class CalculatorRunner {
 
         //test your program here
         //1. create an instance of the calculator class
-
         //2. call any methods of that class to test
+        Calculator myCalc = new Calculator("TI-84 Plus");
+
+        System.out.println(myCalc.info());
+        System.out.println("Add: " + myCalc.performOperation("+", 10, 5));
+        System.out.println("Subtract: " + myCalc.performOperation("-", 10, 5));
+        System.out.println("Multiply: " + myCalc.performOperation("*", 10, 5));
+        System.out.println("Divide: " + myCalc.performOperation("/", 10, 5));
+        System.out.println("Divisible: " + myCalc.divisibleBy(10, 5));
+        System.out.println("Coordinate: " + myCalc.coordinatePair(3, 7));
+        System.out.println("Absolute Value: " + myCalc.absoluteValue(3, 7));
+        System.out.println("Average: " + myCalc.average(7, 8));
 
     }
 }
